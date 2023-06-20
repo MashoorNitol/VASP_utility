ecfda09436b1145e9d99010123dac3c200be12a2
MashroorNitol <43212455+MashoorNitol@users.noreply.github.com>
2023-06-20 09:21:45 -0600

    Create copy_files.py

---
+++ moveVASPfiles/copy_files.py
@@ -0,0 +1,31 @@
+# various folders starting with input_*, moving files from each folder
+import os
+import shutil
+
+# Specify the source directory containing the input folders
+source_directory = './'
+
+# Specify the destination directory to move the renamed files
+destination_directory = '~/Documents/'
+
+# Get a list of all folders in the source directory
+folders = [folder for folder in os.listdir(source_directory) if os.path.isdir(os.path.join(source_directory, folder)) and folder.startswith('input_')]
+
+# Iterate through each folder
+for folder in folders:
+    # Get the folder number by removing the 'input_' prefix
+    folder_number = folder.replace('input_', '')
+
+    # Create the source file paths
+    contcar_source = os.path.join(source_directory, folder, 'CONTCAR')
+    oszicar_source = os.path.join(source_directory, folder, 'OSZICAR')
+
+    # Create the destination file paths with the folder number prefix
+    contcar_destination = os.path.join(destination_directory, f'CONTCAR_{folder_number}')
+    oszicar_destination = os.path.join(destination_directory, f'OSZICAR_{folder_number}')
+
+    # Rename and move the files
+    shutil.copyfile(contcar_source, contcar_destination)
+    shutil.copyfile(oszicar_source, oszicar_destination)
+
+print('Files renamed and moved successfully.')
